dc756031ec8883c5ad4ed98623a847141a49bc99
spal-synaptics <sauryadeep.pal@synaptics.com>
2024-09-11 14:19:35 -0700

    add output dir for .pyz builds

--- video_inference/pyz_builder.py
+++ video_inference/pyz_builder.py
@@ -30,10 +30,17 @@ if __name__ == "__main__":
         choices=["demo.py", *examples],
         help="Which demo(s) to build ([default] %(choices)s)"
     )
+    parser.add_argument(
+        "-o", "--output_dir",
+        type=str,
+        default=f"{getcwd()}/build",
+        metavar="DIR",
+        help="Output directory for builds (default: %(default)s)"
+    )
     args = parser.parse_args()
 
-    if not (exec_dir := Path(cwd / "exec")).exists():
-        exec_dir.mkdir(exist_ok=True)
+    if not (output_dir := Path(args.output_dir)).exists():
+        output_dir.mkdir(parents=True)
 
     targets: list[str] = ["demo.py", *examples] if args.all else args.targets
     for target in targets:
@@ -52,5 +59,5 @@ if __name__ == "__main__":
                     copy2(f, dst)
             target_src: Path = cwd / target if target == "demo.py" else cwd / "examples" / target
             copy2(target_src, td / "__main__.py")
-            zipapp.create_archive(td, f"{cwd}/exec/{Path(target).stem}.pyz")
-            print(f"Built .pyz for {target}")
+            zipapp.create_archive(td, f"{output_dir}/{Path(target).stem}.pyz")
+            print(f"Built {output_dir.resolve()}/{Path(target).stem}.pyz")
